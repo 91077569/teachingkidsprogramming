0e1777e6d9d61fc79de4f941e2a537e793300484
91077569 <91077569@mvusd.net>
2016-10-31 10:47:33 -0700

    HiLowQuiz

--- section03ifs/ChooseYourOwnAdventure.java
+++ section03ifs/ChooseYourOwnAdventure.java
@@ -16,7 +16,7 @@ public class ChooseYourOwnAdventure
   private static void startStory()
   {
     tellMoreStory("One morning the Tortoise woke up in a dream.");
-    animateStartStory();
+    //animateStartStory();
     String action = askAQuestion("Do you want to 'wake up' or 'explore' the dream?");
     if ("wake up".equalsIgnoreCase(action))
     {
@@ -45,7 +45,6 @@ public class ChooseYourOwnAdventure
   private static void approachOoze()
   {
     //      ------------- Recipe for approachOoze --#4.2
-    //         Tell the user "You approach a glowing, green bucket of ooze. Worried that you will get in trouble, you pick up the bucket." --#3
     MessageBox.showMessage(
         "You approach a glowing, green bucket of ooze. Worried that you will get in trouble, you pick up the bucket.");
     String answer = MessageBox.askForTextInput(
@@ -54,10 +53,12 @@ public class ChooseYourOwnAdventure
     //         If they answer "toilet" --#8.1
     if (answer == toilet)
     {
+      
     }
     //            pourIntoToilet (recipe below) --#12.1
     //            ------------- Recipe for pourIntoToilet --#12.2
     //               Tell the user "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste." --#10
+          `         MessageBox.showMessage("As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste.");
     //               Ask the user "Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?" --#11
     //               If they answer "yes" --#13.1
     //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
@@ -67,6 +68,9 @@ public class ChooseYourOwnAdventure
     //                    endStory --#16
     //            ------------- End of pourIntoToilet recipe --#12.3
     //         Otherwise, if they answer "backyard" --#8.2
+    else {
+      
+    }
     //            pourIntoBackyard (recipe below) --#19.1
     //            ------------- Recipe for pourIntoBackyard --#19.2
     //                Tell the user "As you walk into the backyard a net scoops you up and a giant takes you to a boiling pot of water." --#17
@@ -79,7 +83,11 @@ public class ChooseYourOwnAdventure
     //                    endStory --#2
     //         ------------- End of pourIntoBackyard recipe --#19.3
     //         Otherwise, if they answer anything else --#8.3
+    else {
+      
+    }
     //            endStory --#9
+    endStory();
     //      ------------- End of approachOoze recipe --#4.3
   }
   private static void wakeUpLazyBacon()
--- section03ifs/HiLowQuiz.java
+++ section03ifs/HiLowQuiz.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.section03ifs;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 import org.teachingkidsprogramming.recipes.quizzes.graders.HiLowQuizGrader;
 
 public class HiLowQuiz extends org.teachingkidsprogramming.recipes.quizzes.graders.HiLowQuiz
@@ -7,26 +9,35 @@ public class HiLowQuiz extends org.teachingkidsprogramming.recipes.quizzes.grade
   @Override
   public void question1()
   {
-    //  If the Y position of the tortoise is 115
-    //  Turn the tortoise to the right 63 degrees 
+    if (Tortoise.getY() == 115)
+    {
+      Tortoise.turn(63);
+    }
   }
   @Override
   public void question2()
   {
-    //  If the X position of tortoise is less than Y position of tortoise
-    //  Turn the tortoise 54 degrees to the left
-    //  Otherwise turn the tortoise 22 degrees to the right
+    if (Tortoise.getX() < Tortoise.getY())
+    {
+      Tortoise.turn(-54);
+    }
+    else
+    {
+      Tortoise.turn(22);
+    }
   }
   @Override
   public void question3()
   {
-    //  Display the message "elcomeway omehay!"
+    MessageBox.showMessage("elcomeway omehay!");
   }
   @Override
   public void question4()
   {
-    //  If the Y position of tortoise is greater than 50
-    //  Turn the tortoise 177 degrees to the left
+    if (Tortoise.getY() > 50)
+    {
+      Tortoise.turn(-177);
+    }
   }
   public static void main(String[] args)
   {
