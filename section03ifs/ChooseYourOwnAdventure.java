559e268e68f3156cbf3a50109b6f44d7bffd3f22
91077569 <91077569@mvusd.net>
2016-11-02 08:22:47 -0700

    Choose your own adventure edits

--- section03ifs/ChooseYourOwnAdventure.java
+++ section03ifs/ChooseYourOwnAdventure.java
@@ -16,7 +16,7 @@ public class ChooseYourOwnAdventure
   private static void startStory()
   {
     tellMoreStory("One morning the Tortoise woke up in a dream.");
-    //animateStartStory();
+    animateStartStory();
     String action = askAQuestion("Do you want to 'wake up' or 'explore' the dream?");
     if ("wake up".equalsIgnoreCase(action))
     {
@@ -37,7 +37,6 @@ public class ChooseYourOwnAdventure
   private static void endStory()
   {
     //      ------------- Recipe for endStory --#6.2
-    //         Tell the user "You don't know how to read directions. You can't play this game. The end." --#5
     MessageBox.showMessage(
         "You don't know how to read directions. You can't play this game. The end. Oh and we can not be friends");
     //      ------------- End of endStory recipe --#6.3
@@ -47,53 +46,74 @@ public class ChooseYourOwnAdventure
     //      ------------- Recipe for approachOoze --#4.2
     MessageBox.showMessage(
         "You approach a glowing, green bucket of ooze. Worried that you will get in trouble, you pick up the bucket.");
-    String answer = MessageBox.askForTextInput(
-        "Do you want to pour the ooze into the 'backyard' or 'toilet'? Hint umm try toilet? no try backyard?");
-    String toilet = answer;
-    //         If they answer "toilet" --#8.1
-    if (answer == toilet)
+    String input = askAQuestion("Do you want to pour the ooze into the 'backyard' or 'toilet'?");
+    if ("toilet".equalsIgnoreCase(input))
     {
-      
+      //            pourIntoToilet (recipe below) --#12.1
+      pourIntoToilet();
     }
-    //            pourIntoToilet (recipe below) --#12.1
-    //            ------------- Recipe for pourIntoToilet --#12.2
-    //               Tell the user "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste." --#10
-          `         MessageBox.showMessage("As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste.");
-    //               Ask the user "Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?" --#11
-    //               If they answer "yes" --#13.1
-    //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
-    //               Otherwise, if they answer "heck yes" --#13.2
-    //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#15
-    //               Otherwise, if they answer anything else --#13.3
-    //                    endStory --#16
-    //            ------------- End of pourIntoToilet recipe --#12.3
-    //         Otherwise, if they answer "backyard" --#8.2
-    else {
-      
+    else if ("backyard".equalsIgnoreCase(input))
+    {
+      //            pourIntoBackyard (recipe below) --#19.1
+      pourIntoBackyard();
     }
-    //            pourIntoBackyard (recipe below) --#19.1
-    //            ------------- Recipe for pourIntoBackyard --#19.2
-    //                Tell the user "As you walk into the backyard a net scoops you up and a giant takes you to a boiling pot of water." --#17
-    //                Ask the user "As the man starts to prepare you as soup, do you...'Scream' or 'Faint'?" --#18
-    //                If they answer "faint" --#20.1
-    //                        Tell the user "You made a delicious soup! Yum! The end." --#21
-    //                Otherwise, if they answer "scream" --#20.2
-    //                    startStory --#22
-    //                Otherwise, if they answer anything else --#20.3
-    //                    endStory --#2
-    //         ------------- End of pourIntoBackyard recipe --#19.3
-    //         Otherwise, if they answer anything else --#8.3
-    else {
-      
+    else
+    {
+      endStory();
     }
-    //            endStory --#9
-    endStory();
     //      ------------- End of approachOoze recipe --#4.3
+  }
+  private static void pourIntoBackyard()
+  {
+    //            ------------- Recipe for pourIntoBackyard --#19.2
+    MessageBox.showMessage(
+        "As you walk into the backyard a net scoops you up and a giant takes you to a boiling pot of water.");
+    String backyard = askAQuestion("As the man starts to prepare you as soup, do you...'Scream' or 'Faint'?");
+    if ("toilet".equalsIgnoreCase(backyard))
+    {
+      //            pourIntoToilet (recipe below) --#12.1
+      pourIntoToilet();
+    }
+    if ("faint".equalsIgnoreCase(backyard))
+    {
+      MessageBox.showMessage("You made a delicious soup! Yum! The end.");
+    }
+    else if ("scream".equalsIgnoreCase(backyard))
+    {
+    }
+    //                    startStory --#22
+    else
+    {
+      startStory();
+    }
+    endStory();
+    //         ------------- End of pourIntoBackyard recipe --#19.3
+  }
+  private static void pourIntoToilet()
+  {
+    //            ------------- Recipe for pourIntoToilet --#12.2
+    MessageBox.showMessage(
+        "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste.");
+    String toilet = askAQuestion("Do you get bullied then do you want to be a NINJAA? Yes or HECK YES?");
+    if ("yes".equalsIgnoreCase(toilet))
+    {
+      MessageBox
+          .showMessage("Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!");
+    }
+    else if ("heck yes".equalsIgnoreCase(toilet))
+    {
+      MessageBox
+          .showMessage("Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!");
+    }
+    else
+    {
+      endStory();
+    }
+    //            ------------- End of pourIntoToilet recipe --#12.3
   }
   private static void wakeUpLazyBacon()
   {
     //          ------------- Recipe for wakeUp --#2.2
-    //              Tell the user "You wake up and have a boring day. The end." --#1
     MessageBox.showMessage("You wake up.Eat bacon,go to work at mcdonalds,eat beans,watch trumps life story, "
         + "go to bed with your shoelace.WOW that was fun!!!*sarcasm*");
     //          ------------- End of wakeUp recipe --#2.3
