b730c905d56c811a89822643401f2bc038e43b9e
91077569 <91077569@mvusd.net>
2016-11-02 08:23:14 -0700

    Concentric Loops

--- section03ifs/ConcentricLoop.java
+++ section03ifs/ConcentricLoop.java
@@ -1,24 +1,30 @@
 package org.teachingkidsprogramming.section03ifs;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class ConcentricLoop
 {
   public static void main(String[] args)
   {
     Tortoise.show();
-    //  Set the tortoise x position to 150 --#4.1
-    //  Set the tortoise y position to 200 --#4.2
-    //  Set the tortoise speed to 10  --#4.3
-    //  Do the following 360 times --#3.1
-    //      Move the tortoise 3 pixels --#1
-    //      Turn the tortoise 1 degree --#2
-    //      If i is divisible by 20 --(HINT: Use mod operator '%') --#7
-    //          Do the following 360 times --#8.1
-    //              Move the tortoise 1 pixel  --#5  
-    //              Turn the tortoise 1 degree --#6    
-    //              Change the width of the line that the tortoise draws to 1/100th of the current line --#9      
-    //          Repeat --#8.2
-    //   Repeat --#3.2
+    Tortoise.setX(150);
+    Tortoise.setY(200);
+    Tortoise.setSpeed(10);
+    for (int i = 0; i < 360; i++)
+    {
+      Tortoise.move(3);
+      Tortoise.turn(1);
+      if (i % 20 == 0)
+      {
+      }
+      for (int j = 0; j < 360; j++)
+      {
+        Tortoise.move(1);
+        Tortoise.turn(1);
+        Tortoise.setPenWidth(j / 100);
+        Tortoise.setPenColor(PenColors.getRandomColor());
+      }
+    }
   }
 }
