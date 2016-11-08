9baa8ea54a76b6c025221dc076c29fb3c84c256c
91077569 <91077569@mvusd.net>
2016-11-08 09:44:31 -0800

    Pentagon Crazy Quiz

--- section04mastery/PentagonCrazyQuiz.java
+++ section04mastery/PentagonCrazyQuiz.java
@@ -1,5 +1,8 @@
 package org.teachingkidsprogramming.section04mastery;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonCrazyQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonQuizAdapter;
 
@@ -8,27 +11,29 @@ public class PentagonCrazyQuiz extends PentagonQuizAdapter
   public void question1()
   {
     //  Question 1 
-    //  createAStitch (recipe below) 
-    //  ------------- Recipe for createAStitch
-    //  Move the tortoise 6 pixels
-    //  ------------- End of createAStitch recipe
+    createAStitch();
+  }
+  private void createAStitch()
+  {
+    Tortoise.move(6);
   }
   @Override
   public void question2()
   {
-    //  Do the following 76 times
-    //      Call sewAStitch
-    //  Repeat
+    for (int i = 0; i < 76; i++)
+    {
+      sewAStitch();
+    }
   }
   @Override
   public void question3()
   {
-    //  Add lime to the color wheel
+    ColorWheel.addColor(PenColors.Greens.Lime);
   }
   @Override
   public void question4()
   {
-    //  Add red to the color wheel
+    ColorWheel.addColor(PenColors.Reds.Red);
   }
   public static void main(String[] args)
   {
