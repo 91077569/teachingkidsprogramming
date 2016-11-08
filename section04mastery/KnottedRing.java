a5f3e05f554cd26131d29528c73b1efbe6eec522
91077569 <91077569@mvusd.net>
2016-11-08 09:44:44 -0800

    Knotted Ring 1

--- section04mastery/KnottedRing.java
+++ section04mastery/KnottedRing.java
@@ -9,20 +9,24 @@ public class KnottedRing
   public static void main(String[] args)
   {
     Tortoise.show();
-    //      Make the tortoise move as fast as possible --#4
+    Tortoise.setSpeed(10);
     createColorPalette();
-    //          Do the following 30 times --#10.1
-    //              Change the pen color of the line the tortoise draws to the next color from the color wheel --#5
-    //              drawOctagonWithOverlap (recipe below) --#8.0
-    //              ------------- Recipe for drawOctagonWithOverlap --#7.1
-    //                  Do the following 8 + 1 times --#3.1
-    //                        Move the tortoise 110 pixels --#1
-    //                        Turn the tortoise 1/8th of 360 degrees to the right --#2
-    //                  Repeat --#3.2
-    //              ------------- End of drawOctagonWithOverlap recipe --#7.2
-    //       Turn the tortoise 1/30th of 360 degrees to the right --#9
-    //       Turn the tortoise 5 more degrees to the right --#11
-    //       Repeat --#10.2
+    for (int i = 0; i < 30; i++)
+    {
+      Tortoise.setPenColor(ColorWheel.getNextColor());
+      drawOctagonWithOverlap();
+      Tortoise.turn(360 / 30);
+      //       Turn the tortoise 5 more degrees to the right --#11
+      Tortoise.turn(5);
+    }
+  }
+  private static void drawOctagonWithOverlap()
+  {
+    for (int i = 0; i < 9; i++)
+    {
+      Tortoise.move(110);
+      Tortoise.turn(360 / 8);
+    }
   }
   private static void createColorPalette()
   {
