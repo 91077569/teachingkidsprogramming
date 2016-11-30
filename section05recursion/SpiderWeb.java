3de349cffc413f45249748c4c3097f9e16166e6a
91077569 <91077569@mvusd.net>
2016-11-30 08:01:57 -0800

    Spider Web

--- section05recursion/SpiderWeb.java
+++ section05recursion/SpiderWeb.java
@@ -1,45 +1,46 @@
 package org.teachingkidsprogramming.section05recursion;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.virtualproctor.VirtualProctor;
 
 public class SpiderWeb
 {
   public static void main(String[] args)
   {
+    VirtualProctor.setName("Kevin and Dominic");
+    VirtualProctor.setClassName("Palmdigital");
     Tortoise.show();
-    //    Make the tortoise go as fast as possible --#6
-    //    Change the width of the line the tortoise draws to 1 pixel --#12
-    //    Change the Tortoise to a Spider --#14
-    //    Change the pen color of the line the tortoise draws to red --#13.1
-    //    Set the color of the background window to black (HINT: use the Tortoise to get the background window) --#13.2
-    Double length = 10.5;
-    //    The current zoom is 1.1 --#8.2
-    //    Do the following 10 times --#10.1
-    //        weaveOneLayer (recipe below) --#9.1
-    //
-    //        ------------- Recipe for weaveOneLayer --#9.2
-    //        Do the following 6 times --#5.1
-    //            drawTriangle (recipe below) --#4.1
-    //
-    drawTriangle(length);
-    //
-    //            Turn the tortoise 1/6th of 360 degrees to the right --#7
-    //            Increase the current length (of the line) by the current zoom --#8.1
-    //        Repeat --#5.2
-    //        ------------- End of weaveOneLayer recipe --#9.3
-    //
-    //        Change the current zoom so it is multiplied by 1.3 --#11
-    //    Repeat --#10.2
+    Tortoise.setSpeed(10);
+    Tortoise.setPenWidth(1);
+    Tortoise.setAnimal(Animals.Spider);
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.getBackgroundWindow().setBackground(PenColors.Grays.Black);
+    float length = 10.5f;
+    float zoom = 1.1f;
+    for (int i = 0; i < 10; i++)
+    {
+      length = weaveOneLayer(length, zoom);
+      zoom = zoom * 1.3f;
+    }
   }
-
-  private static void drawTriangle(Double length)
+  private static float weaveOneLayer(float length, float zoom)
   {
-    //            ------------- Recipe for drawTriangle --#4.2
+    for (int i = 0; i < 6; i++)
+    {
+      drawTriangle(length);
+      Tortoise.turn(360 / 6);
+      length = length + zoom;
+    }
+    return length;
+  }
+  private static void drawTriangle(float length)
+  {
     for (int i = 0; i < 3; i++)
     {
       Tortoise.move(length);
       Tortoise.turn(360 / 3);
     }
-    //            ------------- End of drawTriangle recipe --#4.3
   }
 }
